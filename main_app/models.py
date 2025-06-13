4da581f12b64e3fc50316e3bef2035313cd3ade3
gerol-r <gerol.revills@gmail.com>
2025-06-13 16:55:18 -0400

    custom user model

--- main_app/models.py
+++ main_app/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class User(AbstractUser):
     # Inherit username, email, password, etc.
-    coins = models.DecimalField(max_digits=10, decimal_places=2, default=5000.00)  # Set an initial coin balance as needed
+    coins = models.DecimalField(max_digits=10, decimal_places=2, default=5000.00)
     lottery_used = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
